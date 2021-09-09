24d488894f682107b6de4ce2c5ba31ce88f3a2e5
Yuanqing Wu <yuanqingwu@whu.edu.cn>
2021-09-09 22:04:10 +0800

    增加了网页端登录功能

--- HTML/exam/examScore.php
+++ HTML/exam/examScore.php
@@ -3,6 +3,15 @@
 $answer = json_decode($_GET["answer"], true);
 $correct = array("B", "B", "B", "B", "B", "C", "D", "D", "D", "D");
 
+$server = "localhost";
+$username = "XRD";
+$passwd = "27mZFsmWzkiTeGsB";
+$conn = new mysqli($server, $username, $passwd, "xrd");
+if ($conn->connect_error) {
+	die("无法连接到数据库, 请联系管理员:pingline@outlook.com. Details:" . $conn->connect_error);
+}
+
+$details = "";
 $score = 0;
 for ($x=0; $x<10; $x++)
 {
@@ -15,8 +24,16 @@ for ($x=0; $x<10; $x++)
 	}
 	else
 	{
-		echo "第" . ($x+1) ."题错误, 正确选项为:" . $correct[$x] . "\n";
+		//echo "\"第" . ($x+1) ."题错误, 正确选项为:" . $correct[$x];
+		$details = $details . "第" . ($x+1) . "题错误, 正确的选项为:" . $correct[$x] . "\n";
 	}
 }
-echo "您已完成预习题, 得分为:".(string)$score."分";
+if ($_COOKIE["type"]=="guest")
+{
+	mysqli_query($conn, "UPDATE `xrd`.`guest` SET `pre_score` = '".$score."', `pre_details` = '".$details."' WHERE `guest`.`id` = ".$_COOKIE["id"].";");
+}
+//echo $_COOKIE["type"];
+//echo $details;
+echo "最终成绩为:".$score."分";
+$conn->close();
 ?>
